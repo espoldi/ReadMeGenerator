ed2d4946e124ca916801c481f581edb19da20633
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-09 18:54:59 -0500

    added questions for user input

--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const util = require('util');
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of this project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please briefly describe this project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install this project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is this project meant to be used for?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Which license applies?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who and what were contributing people and/or technologies?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Any tests available for this project?',
+    },
+    {
+        type: 'input',
+        name: 'user',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
